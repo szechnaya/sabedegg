0394741478bbbef23775162a52df0f02c119023d
szechnaya <134574988+szechnaya@users.noreply.github.com>
2024-06-01 13:03:32 +0700

    Update ggset.ps1

--- ggset.ps1
+++ ggset.ps1
@@ -1,4 +1,3 @@
 Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
-& {$P = $env:TEMP + '\chromeremotedesktophost.msi'; Invoke-WebRequest 'https://dl.google.com/edgedl/chrome-remote-desktop/chromeremotedesktophost.msi' -OutFile $P; Start-Process -Filepath $P -Wait; Remove-Item $P}
 & {$P = $env:TEMP + '\ChromeSetup.exe'; Invoke-WebRequest 'https://dl.google.com/chrome/install/latest/chrome_installer.exe' -OutFile $P; Start-Process -FilePath $P -Args '/install' -Verb RunAs -Wait; Remove-Item $P}
  
